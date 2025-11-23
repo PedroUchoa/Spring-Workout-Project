ca6e3e61a7ba49068586af331612f818b8c93b60
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2025-11-23 19:45:08 -0300

    Add: First Configurations

---
+++ src/main/java/com/pedro/workoutproject/WorkoutprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.workoutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/workoutproject/WorkoutprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.workoutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
