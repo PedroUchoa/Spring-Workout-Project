a2747e8c7c6927a99d93bf5b59699b908850490a
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2025-12-02 21:29:10 -0300

    Add: New Migrations For Db Population

---
+++ src/main/resources/db/migration/V6__insert_into_table_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (id, email, password, role, is_active, created_on)
+VALUES ('ba13fd15-2147-409a-8bd1-a2f5724afb4',
+'admin@email.com',
+'$2a$12$jYokLJWf2I6N0JCxsy6i0O1PboUKg.379FAfhoAAFQHF4umEUJaoy',
+'ADMIN',
+TRUE,
+CURRENT_TIMESTAMP
+);
---
+++ src/main/resources/db/migration/V7__insert_into_table_exercise.sql
@@ -0,0 +1,41 @@
+INSERT INTO exercise (id, name, type, is_active) VALUES
+    (UUID(), 'Supino Reto', 'Peito', 1),
+    (UUID(), 'Supino Inclinado', 'Peito', 1),
+    (UUID(), 'Supino Declinado', 'Peito', 1),
+    (UUID(), 'Crucifixo', 'Peito', 1),
+    (UUID(), 'Crossover', 'Peito', 1),
+
+    (UUID(), 'Puxada na Barra Fixa', 'Costas', 1),
+    (UUID(), 'Puxada na Frente (Polia)', 'Costas', 1),
+    (UUID(), 'Remada Curvada', 'Costas', 1),
+    (UUID(), 'Remada Baixa', 'Costas', 1),
+    (UUID(), 'Remada Unilateral', 'Costas', 1),
+
+    (UUID(), 'Agachamento Livre', 'Pernas', 1),
+    (UUID(), 'Leg Press', 'Pernas', 1),
+    (UUID(), 'Cadeira Extensora', 'Quadríceps', 1),
+    (UUID(), 'Cadeira Flexora', 'Posterior de Coxa', 1),
+    (UUID(), 'Mesa Flexora', 'Posterior de Coxa', 1),
+    (UUID(), 'Panturrilha em Pé', 'Panturrilha', 1),
+    (UUID(), 'Afundo (Lunge)', 'Pernas', 1),
+
+    (UUID(), 'Desenvolvimento com Halteres', 'Ombros', 1),
+    (UUID(), 'Elevação Lateral', 'Ombros', 1),
+    (UUID(), 'Elevação Frontal', 'Ombros', 1),
+    (UUID(), 'Remada Alta', 'Ombros', 1),
+
+    (UUID(), 'Rosca Direta', 'Bíceps', 1),
+    (UUID(), 'Rosca Alternada', 'Bíceps', 1),
+    (UUID(), 'Rosca Martelo', 'Bíceps', 1),
+
+    (UUID(), 'Tríceps Testa', 'Tríceps', 1),
+    (UUID(), 'Tríceps Pulley', 'Tríceps', 1),
+    (UUID(), 'Mergulho entre Bancos', 'Tríceps', 1),
+
+    (UUID(), 'Glúteo Máquina', 'Glúteos', 1),
+    (UUID(), 'Elevação de Quadril (Hip Thrust)', 'Glúteos', 1),
+    (UUID(), 'Agachamento Sumô', 'Glúteos', 1),
+
+    (UUID(), 'Abdominal Supra', 'Abdômen', 1),
+    (UUID(), 'Abdominal Infra', 'Abdômen', 1),
+    (UUID(), 'Prancha', 'Abdômen', 1);
